52283903e5d7462d28058ed66eac53e09938e8aa
De-Alchmst <PolakJ_37@proton.me>
2025-07-23 16:15:02 +0200

    :facepalm:

--- galgen.rb
+++ galgen.rb
@@ -19,7 +19,7 @@ def template(css_path, title, desc, links, images)
   <head>
     <title>#{title}</title>
     <meta charset='UTF-8'>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     <link rel='stylesheet' href='#{css_path}/style.css'>
   </head>
   <body>
